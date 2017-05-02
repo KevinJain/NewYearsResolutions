9fac4dff5bf0334f3c1afb73d1c4a1fe8e5a5d60
Anthony Astige <anthonyastige@gmail.com>
2017-05-02 09:43:25 -0500

    Plans have versions

--- imports/api/resolution_plans/resolution-plans.js
+++ imports/api/resolution_plans/resolution-plans.js
@@ -2,11 +2,10 @@ import { Class } from 'meteor/jagi:astronomy'
 import { Enum } from 'meteor/jagi:astronomy'
 
 const ProofType = Enum.create({
-	name: 'ProofTypes',
+	name: 'ProofType',
 	identifiers: ['BOOLEAN']
-		// TODO: Add support for these proof types later
-		// 'IMAGE', 'VIDEO', 'AUDIO'
-	}
+	// TODO: Add support for these proof types later
+	// 'IMAGE', 'VIDEO', 'AUDIO'
 })
 
 const Frequency = Enum.create({
@@ -48,6 +47,34 @@ const ResolutionPlan = Class.create({
 		_id: {
 			type: Mongo.ObjectID
 		},
+		planId: {
+			// A plan identifier to Group together mulitple versions of the same plan
+			type: String,
+			validators: [
+				{
+					type: 'required',
+				}
+			]
+		},
+		// A ResolutionLog depends on a particular version of a ResolutionPlan
+		// * ResolutionPlans may be drafted and updated
+		// TODO: Add validation only allow editing version -1
+		// TODO: Think through semver logic
+		// TODO: * Add validation allow creating a newer version than latest?
+		// TODO: * Maybe can find semver package for this logic?
+		version: {
+			// For now any version update will be incompatable with previous
+			// Only show students ResolutionPlans
+			// * With version >= 0
+			// * Of the greatest version in a set of planId
+			type: 'Number',
+			// TODO: Add integer validation? Or Semver?
+			// TODO: * If semver will need minor version additions:
+			// TODO: ** Enforcement where possible like no removing or re-ordering steps, but allow adding
+			// TODO: ** UI explaining non-breaking change
+			// TODO: * Allow version -1 for the current draft, but all other must be > 0?
+			default: () => -1
+		},
 		proofTypes: {
 			type: ProofType
 			default: () => ProofType.BOOLEAN
@@ -56,10 +83,10 @@ const ResolutionPlan = Class.create({
 			type: Frequency
 			default: () => Frequency.DAILY
 		},
-		taskSeries: {
+		taskSteps: {
 			type: [Task],
 			default: () => []
-		}
+		},
 		title: {
 			type: String,
 			validators: [
@@ -83,6 +110,18 @@ const ResolutionPlan = Class.create({
 		updatedAt: Date
 		// TODO: Add field to connect with Stripe?
 	},
+	indexes: {
+		// Only one version for each plan
+		idVersions: {
+			fields: [
+				planId: 1,
+				version: 1
+			],
+			options: {
+				unique: true
+			}
+		}
+	}
 	behaviors: {
 		timestamp: {
 			hasCreatedField: true,
