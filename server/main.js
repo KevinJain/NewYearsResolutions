cd7b74d399df8b2c39405c3ce7033786475498b2
Anthony Astige <anthonyastige@gmail.com>
2017-05-05 07:19:56 -0500

    Allow YouTube video embeds / iframes

--- server/main.js
+++ server/main.js
@@ -3,4 +3,5 @@ import { BrowserPolicy } from 'meteor/browser-policy';
 
 Meteor.startup(() => {
 	BrowserPolicy.content.allowOriginForAll("https://js.stripe.com/")
+	BrowserPolicy.content.allowOriginForAll("https://www.youtube.com/")
 });
