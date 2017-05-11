d9abacfec2f0069cc4844f3db0be95f3d5205eaf
Anthony Astige <anthonyastige@gmail.com>
2017-05-11 08:47:43 -0500

    Calendar: Pass events

--- imports/ui/pages/CalendarPage.jsx
+++ imports/ui/pages/CalendarPage.jsx
@@ -17,77 +17,6 @@ import { User } from '../../api/users/users.js'
 // to the correct localizer.
 BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
-const events = [
-  {
-    'title': 'All Day Event',
-    'allDay': true,
-    'start': new Date(2015, 3, 0),
-    'end': new Date(2015, 3, 1)
-  },
-  {
-    'title': 'Long Event',
-    'start': new Date(2015, 3, 7),
-    'end': new Date(2015, 3, 10)
-  },
-
-  {
-    'title': 'DTS STARTS',
-    'start': new Date(2016, 2, 13, 0, 0, 0),
-    'end': new Date(2016, 2, 20, 0, 0, 0)
-  },
-
-  {
-    'title': 'DTS ENDS',
-    'start': new Date(2016, 10, 6, 0, 0, 0),
-    'end': new Date(2016, 10, 13, 0, 0, 0)
-  },
-
-  {
-    'title': 'Some Event',
-    'start': new Date(2015, 3, 9, 0, 0, 0),
-    'end': new Date(2015, 3, 9, 0, 0, 0)
-  },
-  {
-    'title': 'Conference',
-    'start': new Date(2015, 3, 11),
-    'end': new Date(2015, 3, 13),
-    desc: 'Big conference for important people'
-  },
-  {
-    'title': 'Meeting',
-    'start': new Date(2015, 3, 12, 10, 30, 0, 0),
-    'end': new Date(2015, 3, 12, 12, 30, 0, 0),
-    desc: 'Pre-meeting meeting, to prepare for the meeting'
-  },
-  {
-    'title': 'Lunch',
-    'start':new Date(2015, 3, 12, 12, 0, 0, 0),
-    'end': new Date(2015, 3, 12, 13, 0, 0, 0),
-    desc: 'Power lunch'
-  },
-  {
-    'title': 'Meeting',
-    'start':new Date(2015, 3, 12,14, 0, 0, 0),
-    'end': new Date(2015, 3, 12,15, 0, 0, 0)
-  },
-  {
-    'title': 'Happy Hour',
-    'start':new Date(2015, 3, 12, 17, 0, 0, 0),
-    'end': new Date(2015, 3, 12, 17, 30, 0, 0),
-    desc: 'Most important meal of the day'
-  },
-  {
-    'title': 'Dinner',
-    'start':new Date(2015, 3, 12, 20, 0, 0, 0),
-    'end': new Date(2015, 3, 12, 21, 0, 0, 0)
-  },
-  {
-    'title': 'Birthday Party',
-    'start':new Date(2015, 3, 13, 7, 0, 0),
-    'end': new Date(2015, 3, 13, 10, 30, 0)
-  }
-]
-
 class ResolutionCalendar extends BaseComponent {
 	constructor(props) {
 		super(props);
@@ -96,10 +25,10 @@ class ResolutionCalendar extends BaseComponent {
 
 	render() {
 		return (
-			<div {...this.props}>
+			<div>
 				<BigCalendar
 					selectable
-					events={events}
+					events={this.props.events}
 					defaultView='month'
 					scrollToTime={new Date(1970, 1, 1, 6)}
 					defaultDate={new Date(2015, 3, 12)}
@@ -187,11 +116,82 @@ export default class CalendarPage extends BaseComponent {
 		)
 		*/
 
+		const events = [
+		  {
+			'title': 'All Day Event',
+			'allDay': true,
+			'start': new Date(2015, 3, 0),
+			'end': new Date(2015, 3, 1)
+		  },
+		  {
+			'title': 'Long Event',
+			'start': new Date(2015, 3, 7),
+			'end': new Date(2015, 3, 10)
+		  },
+
+		  {
+			'title': 'DTS STARTS',
+			'start': new Date(2016, 2, 13, 0, 0, 0),
+			'end': new Date(2016, 2, 20, 0, 0, 0)
+		  },
+
+		  {
+			'title': 'DTS ENDS',
+			'start': new Date(2016, 10, 6, 0, 0, 0),
+			'end': new Date(2016, 10, 13, 0, 0, 0)
+		  },
+
+		  {
+			'title': 'Some Event',
+			'start': new Date(2015, 3, 9, 0, 0, 0),
+			'end': new Date(2015, 3, 9, 0, 0, 0)
+		  },
+		  {
+			'title': 'Conference',
+			'start': new Date(2015, 3, 11),
+			'end': new Date(2015, 3, 13),
+			desc: 'Big conference for important people'
+		  },
+		  {
+			'title': 'Meeting',
+			'start': new Date(2015, 3, 12, 10, 30, 0, 0),
+			'end': new Date(2015, 3, 12, 12, 30, 0, 0),
+			desc: 'Pre-meeting meeting, to prepare for the meeting'
+		  },
+		  {
+			'title': 'Lunch',
+			'start':new Date(2015, 3, 12, 12, 0, 0, 0),
+			'end': new Date(2015, 3, 12, 13, 0, 0, 0),
+			desc: 'Power lunch'
+		  },
+		  {
+			'title': 'Meeting',
+			'start':new Date(2015, 3, 12,14, 0, 0, 0),
+			'end': new Date(2015, 3, 12,15, 0, 0, 0)
+		  },
+		  {
+			'title': 'Happy Hour',
+			'start':new Date(2015, 3, 12, 17, 0, 0, 0),
+			'end': new Date(2015, 3, 12, 17, 30, 0, 0),
+			desc: 'Most important meal of the day'
+		  },
+		  {
+			'title': 'Dinner',
+			'start':new Date(2015, 3, 12, 20, 0, 0, 0),
+			'end': new Date(2015, 3, 12, 21, 0, 0, 0)
+		  },
+		  {
+			'title': 'Birthday Party',
+			'start':new Date(2015, 3, 13, 7, 0, 0),
+			'end': new Date(2015, 3, 13, 10, 30, 0)
+		  }
+		]
+
 		const content = (
 			<div>
 				<h1>{i18n.__('pages.calendarPage.title')}</h1>
 				<h3>{i18n.__('pages.calendarPage.subTitle')}</h3>
-				<ResolutionCalendar />
+				<ResolutionCalendar events={events} />
 			</div>
 		)
 
