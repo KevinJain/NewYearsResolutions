4530cab46306932930f932dfa6dd7af87664e1af
Anthony Astige <anthonyastige@gmail.com>
2017-05-11 08:28:35 -0500

    Proof of concept ReactBigCalendar on page

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/startup/client/routes.jsx';
+import "../node_modules/react-big-calendar/lib/css/react-big-calendar.css"
 
 Meteor.startup(() => {
   // Global subscriptions ; data we always want availble
--- imports/ui/pages/CalendarPage.jsx
+++ imports/ui/pages/CalendarPage.jsx
@@ -9,8 +9,109 @@ import BaseComponent from '../components/BaseComponent.jsx';
 import { ResolutionPlan } from '../../api/resolution_plans/resolution-plans.js';
 import { ResolutionLog } from '../../api/resolution_logs/resolution-logs.js';
 import { ResolutionLogsHelpers } from '../../api/resolution_logs/resolution-logs.js';
+import BigCalendar from 'react-big-calendar';
 
 import { User } from '../../api/users/users.js'
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+
+const events = [
+  {
+    'title': 'All Day Event',
+    'allDay': true,
+    'start': new Date(2015, 3, 0),
+    'end': new Date(2015, 3, 1)
+  },
+  {
+    'title': 'Long Event',
+    'start': new Date(2015, 3, 7),
+    'end': new Date(2015, 3, 10)
+  },
+
+  {
+    'title': 'DTS STARTS',
+    'start': new Date(2016, 2, 13, 0, 0, 0),
+    'end': new Date(2016, 2, 20, 0, 0, 0)
+  },
+
+  {
+    'title': 'DTS ENDS',
+    'start': new Date(2016, 10, 6, 0, 0, 0),
+    'end': new Date(2016, 10, 13, 0, 0, 0)
+  },
+
+  {
+    'title': 'Some Event',
+    'start': new Date(2015, 3, 9, 0, 0, 0),
+    'end': new Date(2015, 3, 9, 0, 0, 0)
+  },
+  {
+    'title': 'Conference',
+    'start': new Date(2015, 3, 11),
+    'end': new Date(2015, 3, 13),
+    desc: 'Big conference for important people'
+  },
+  {
+    'title': 'Meeting',
+    'start': new Date(2015, 3, 12, 10, 30, 0, 0),
+    'end': new Date(2015, 3, 12, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to prepare for the meeting'
+  },
+  {
+    'title': 'Lunch',
+    'start':new Date(2015, 3, 12, 12, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch'
+  },
+  {
+    'title': 'Meeting',
+    'start':new Date(2015, 3, 12,14, 0, 0, 0),
+    'end': new Date(2015, 3, 12,15, 0, 0, 0)
+  },
+  {
+    'title': 'Happy Hour',
+    'start':new Date(2015, 3, 12, 17, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day'
+  },
+  {
+    'title': 'Dinner',
+    'start':new Date(2015, 3, 12, 20, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 21, 0, 0, 0)
+  },
+  {
+    'title': 'Birthday Party',
+    'start':new Date(2015, 3, 13, 7, 0, 0),
+    'end': new Date(2015, 3, 13, 10, 30, 0)
+  }
+]
+
+let ResolutionCalendar = React.createClass({
+	render(){
+		return (
+			<div {...this.props}>
+				<h3 className="callout">
+					Click an event to see more info, or
+					drag the mouse over the calendar to select a date/time range.
+				</h3>
+				<BigCalendar
+					selectable
+					events={events}
+					defaultView='month'
+					scrollToTime={new Date(1970, 1, 1, 6)}
+					defaultDate={new Date(2015, 3, 12)}
+					onSelectEvent={event => alert(event.title)}
+					onSelectSlot={(slotInfo) => alert(
+						`selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+						`\nend: ${slotInfo.end.toLocaleString()}`
+					)}
+				/>
+			</div>
+		)
+	}
+})
 
 export default class CalendarPage extends BaseComponent {
 	constructor(props) {
@@ -89,6 +190,7 @@ export default class CalendarPage extends BaseComponent {
 				<h1>
 					Calendar page placeholder
 				</h1>
+				<ResolutionCalendar />
 			</div>
 		)
 
