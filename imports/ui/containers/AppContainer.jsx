2235eb374e05da29c2f5698f39e5e384ae366c22
Anthony Astige <anthonyastige@gmail.com>
2017-05-17 10:37:56 -0500

    Move subscriptions to container for loading indicator

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -2,17 +2,20 @@ import App from '../layouts/App.jsx'
 import { Meteor } from 'meteor/meteor'
 // TODO: no-session!
 import { Session } from 'meteor/session'
+import _ from 'lodash'
 import { createContainer } from 'meteor/react-meteor-data'
 
-export default createContainer(() => (
-	// TODO: Add our loading here
-	// const publicHandle = Meteor.subscribe('lists.public');
-	// const privateHandle = Meteor.subscribe('lists.private');
-	{
+export default createContainer(() => {
+	const subscriptions = [
+		Meteor.subscribe('userData'),
+		Meteor.subscribe('resolutionPlansAll'),
+		Meteor.subscribe('resolutionLogsUser')
+	]
+
+	return {
 		user: Meteor.user(),
-		// loading: !(publicHandle.ready() && privateHandle.ready()),
-		loading: false,
+		loading: !_.every(subscriptions, subscription => subscription.ready()),
 		connected: Meteor.status().connected,
 		menuOpen: Session.get('menuOpen') // eslint-disable-line meteor/no-session
 	}
-), App)
+}, App)
