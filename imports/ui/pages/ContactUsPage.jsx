8c4cb72d5a18b813ad15897680f4e52dc39dcdf6
Anthony Astige <anthonyastige@gmail.com>
2017-06-01 14:48:11 -0500

    Contact us page placeholder content

--- imports/ui/pages/ContactUsPage.jsx
+++ imports/ui/pages/ContactUsPage.jsx
@@ -13,6 +13,18 @@ class ContactUsPage extends BaseComponent {
 				<div className="content-scrollable">
 					<h2>{i18n.__('pages.contactUs.title')}</h2>
 					<div>{i18n.__('pages.contactUs.mission')}</div>
+					<h2>Leave a note</h2>
+					<ul>
+						<li>Feature requests</li>
+						<li>Things you like</li>
+						<li>Other Feedback</li>
+					</ul>
+					{/*
+					<form>
+						<textarea></textarea>
+						<input type="submit" />
+					<form>
+					*/}
 				</div>
 			</div>
 		)
